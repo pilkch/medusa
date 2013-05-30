4f1cbed79f26988d387a12efcb9080b74718c343
Chris Pilkington <chris.pilkington@gmail.com>
2013-05-31 07:13:36 +1000

    main Send output to KDevelop.

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,22 @@ int main(int argc, char* argv[])
 {
   std::cout<<"main"<<std::endl;
 
-  spitfire::util::SetMainThread();
+  #if defined(BUILD_DEBUG) && defined(PLATFORM_LINUX_OR_UNIX)
+  // KDevelop only shows output sent to cerr, we redirect cout to cerr here so that it will show up
+  // Back up cout's streambuf
+  std::streambuf* backup = std::cout.rdbuf();
 
-  spitfire::string::Init();
+  // Assign cerr's streambuf to cout
+  std::cout.rdbuf(std::cerr.rdbuf());
+  #endif
+
+  bool bIsSuccess = false;
 
   {
+    spitfire::util::SetMainThread();
+
+    spitfire::string::Init();
+
     medusa::cModel model;
     medusa::cGtkmmView view(argc, argv);
     medusa::cController controller(model, view);
@@ -28,5 +39,10 @@ int main(int argc, char* argv[])
     view.Run();
   }
 
-  return 0;
+  #if defined(BUILD_DEBUG) && defined(PLATFORM_LINUX_OR_UNIX)
+  // Restore the original cout streambuf
+  std::cout.rdbuf(backup);
+  #endif
+
+  return bIsSuccess ? EXIT_SUCCESS : EXIT_FAILURE;
 }
