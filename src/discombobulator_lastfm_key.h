7f79f297c0577a1ee140990b4a9ceac0913ab0c0
Chris Pilkington <chris.pilkington@gmail.com>
2013-07-06 23:30:11 +1000

    Reverted last.fm key (Secret is still safe).

--- src/discombobulator_lastfm_key.h
+++ src/discombobulator_lastfm_key.h
@@ -15,41 +15,8 @@ namespace discombobulator
 {
   inline std::string GetSecretLastfmKeyUTF8()
   {
-    std::ostringstream o;
-    o<<char(uint8_t(117) + uint8_t(190));
-    o<<char(uint8_t(94) + uint8_t(215));
-    o<<char(uint8_t(160) + uint8_t(147));
-    o<<char(uint8_t(6) + uint8_t(94));
-    o<<char(uint8_t(146) + uint8_t(165));
-    o<<char(uint8_t(58) + uint8_t(252));
-    o<<char(uint8_t(146) + uint8_t(160));
-    o<<char(uint8_t(160) + uint8_t(193));
-    o<<char(uint8_t(181) + uint8_t(123));
-    o<<char(uint8_t(203) + uint8_t(106));
-    o<<char(uint8_t(106) + uint8_t(205));
-    o<<char(uint8_t(105) + uint8_t(204));
-    o<<char(uint8_t(48) + uint8_t(1));
-    o<<char(uint8_t(201) + uint8_t(153));
-    o<<char(uint8_t(185) + uint8_t(124));
-    o<<char(uint8_t(232) + uint8_t(75));
-    o<<char(uint8_t(90) + uint8_t(216));
-    o<<char(uint8_t(155) + uint8_t(155));
-    o<<char(uint8_t(34) + uint8_t(21));
-    o<<char(uint8_t(50) + uint8_t(49));
-    o<<char(uint8_t(12) + uint8_t(37));
-    o<<char(uint8_t(134) + uint8_t(174));
-    o<<char(uint8_t(254) + uint8_t(53));
-    o<<char(uint8_t(29) + uint8_t(23));
-    o<<char(uint8_t(114) + uint8_t(239));
-    o<<char(uint8_t(54) + uint8_t(44));
-    o<<char(uint8_t(139) + uint8_t(215));
-    o<<char(uint8_t(221) + uint8_t(88));
-    o<<char(uint8_t(78) + uint8_t(235));
-    o<<char(uint8_t(158) + uint8_t(199));
-    o<<char(uint8_t(107) + uint8_t(249));
-    o<<char(uint8_t(134) + uint8_t(171));
-
-    return o.str();
+    #error "Your discombobulated key would go here, otherwise you can just remove this line and return an empty string if you don't want last.fm support"
+    return "";
   }
 }
 
